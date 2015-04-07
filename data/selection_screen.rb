8ed4f1069c88a037f08fed2b4e1c6db196919634
Bradly Swart <brad8711@gmail.com>
2015-04-07 15:47:09 +1000

    readme

---
+++ data/selection_screen.rb
@@ -0,0 +1,14 @@
+# role selection
+class SelectionScreen
+  def initialize(trait, ui, options)
+    @items    = trait.for_options(options)
+    @ui       = ui
+    @options  = options
+    @key      = trait.name.downcase.to_sym
+    @messages = Messages[key]
+  end
+
+  private
+
+  attr_reader :items, :ui, :options, :key, :messages
+end
